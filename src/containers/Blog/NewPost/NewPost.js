6b58f638b02e781f6fd92081b46dab26174ce02a
Adam Morsi <adam.morsi@yahoo.com>
2018-06-27 18:05:21 +0300

    Until The withRouter HOC & Route Props

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 import './NewPost.css';
@@ -10,7 +10,9 @@ class NewPost extends Component {
         author: 'Max'
     };
 
-
+    componentDidMount() {
+        console.log(this.props);
+    }
 
     postDataHandler = () => {
         const post = {
@@ -25,14 +27,16 @@ class NewPost extends Component {
             });
     };
 
-    render () {
+    render() {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title}
+                       onChange={(event) => this.setState({title: event.target.value})}/>
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content}
+                          onChange={(event) => this.setState({content: event.target.value})}/>
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
